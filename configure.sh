968cdbe5a35762c4c294e06a413d7114488e2f9b
Kevin Darmon <kdarmondev@gmail.com>
2018-06-05 16:04:36 +0200

    Mac Update from jldeen
    Fixed git credential config bug (global & local) (previously cached my username)
    Fixed copy bug in tmux mode when using jldeen dotfiles
    Fixed insert mode error in vim (allow backspacing over everything in insert mode)
    Fixed pip for setuptools upgrade
    Added checks in for zsh, oh-my-zsh install, and vs code symlink
    Added new features embedded with Powerlevel9k
    - Dir_writable
    - Kubernetes support
    Added iTerm and Azure CLI installs to brewfile for autoinstall
    Removed hardcode source from .zshrc and .bashrc for az
    Updated auto sourcing for zsh-autosuggestions plugin
    Updated settings.json for VS Code
    Updated iterm config
    Streamlined and enhanced bandwidth data performance
    - Speedtest-cli command moved to crontab to be run in 15min increments and output data to log
    - Tmux internet_info.sh reads log and prints relevant data
    General code cleanup and optimization

--- configure.sh
+++ configure.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
 # Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! $(which brew)
+then
+echo "  Installing Homebrew for you."
+
+# Install the correct homebrew for each OS type
+if test "$(uname)" = "Darwin"
+then
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+fi
+
+fi
 
 echo "Updating package lists..."
 brew update
+
 # zsh install
+if test $(which zsh)
+then
 echo ''
-echo "Now installing zsh..."
+echo "zsh already installed..."
+else
+echo "zsh not found, now installing zsh..."
 echo ''
 brew install zsh zsh-completions
+fi
 
 # Installing git completion
 echo ''
@@ -23,14 +42,29 @@ URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-complet
 echo ''
 echo "Downloading git-completion for git version: $GIT_VERSION..."
 if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
-	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
+echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
 fi
 
 # oh-my-zsh install
+if [ -d ~/.oh-my-zsh/ ] ; then
 echo ''
-echo "Now installing oh-my-zsh..."
+echo "oh-my-zsh is already installed..."
+read -p "Would you like to update oh-my-zsh now?" -n 1 -r
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+cd ~/.oh-my-zsh && git pull
+if [[ $? -eq 0 ]]
+then
+echo "Update complete..." && cd
+else
+echo "Update not complete..." >&2 cd
+fi
+fi
+else
+echo "oh-my-zsh not found, now installing oh-my-zsh..."
 echo ''
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # oh-my-zsh plugin install
 echo ''
@@ -57,7 +91,7 @@ echo ''
 echo "Now installing Pathogen..."
 echo ''
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Nerdtree for vim install
 echo ''
@@ -69,7 +103,7 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 echo ''
 echo "Now installing vim wombat color scheme..."
 echo ''
-git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
+git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat
 mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
 
 # Midnight commander install
@@ -84,74 +118,33 @@ read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo ''
-	echo "Now pulling down jldeen dotfiles..."
-	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
-	echo ''
-	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-	echo ''
-	echo "Checking out macOS branch..." && git checkout mac
-	echo ''
-	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
-    echo ''
-    echo "Now setting up dependencies..."
-    sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
-    
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully configured your environment with jldeen's macOS dotfiles..."
-    else
-        echo "jldeen's macOS dotfiles were not applied successfully..." >&2
-fi
-else 
-	echo ''
-    echo "You chose not to apply jldeen's macOS dotfiles. You will need to configure your environment manually..."
-	echo ''
-	echo "Setting defaults for .zshrc and .bashrc..."
-	echo ''
-	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
-	echo ''
-	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
-	echo ''
-	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
-	
-fi
+echo ''
+echo "Now pulling down jldeen dotfiles..."
+git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
+echo ''
+cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+echo ''
+echo "Checking out macOS branch..." && git checkout mac
+echo ''
+echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+echo ''
 
-# Set default shell to zsh
-echo ''
-read -p "Do you want to change your default shell? y/n" -n 1 -r
-echo ''
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $? -eq 0 ]]
 then
-	echo "Now setting default shell..."
-    chsh -s $(which zsh)
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully set your default shell to zsh..."
-    else
-        echo "Default shell not set successfully..." >&2
+echo "Successfully configured your environment with jldeen's macOS dotfiles..."
+else
+echo "jldeen's macOS dotfiles were not applied successfully..." >&2
 fi
-else 
-    echo "You chose not to set your default shell to zsh. Exiting now..."
-fi
+else
+echo ''
+echo "You chose not to apply jldeen's macOS dotfiles. You will need to configure your environment manually..."
+echo ''
+echo "Setting defaults for .zshrc and .bashrc..."
+echo ''
+echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+echo ''
+echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
+echo ''
+echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
 
-# Setup and configure az cli
-echo ''
-read -p "Do you want to install Azure CLI? y/n (This will take some time...)" -n 1 -r
-echo ''
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Now installing az cli..."
-    brew install azure-cli
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully installed Azure CLI 2.0."
-    else
-        echo "Azure CLI not installed successfully." >&2
 fi
-else 
-    echo "You chose not to install Azure CLI. Exiting now."
-fi
-
-echo ''
-echo '	Badass macOS terminal installed!'
