b807825c55428bdb5a34e4e08977697f05dcfee9
KDTheory <kdarmondev@gmail.com>
2019-06-09 18:33:32 +0200

    Update Jldeen fix
    Add some fixs from jldeen dotfiles

--- configure.sh
+++ configure.sh
@@ -3,16 +3,16 @@
 # Install brew
 if test ! $(which brew)
 then
-echo "  Installing Homebrew for you."
+  echo "  Installing Homebrew for you."
 
-# Install the correct homebrew for each OS type
-if test "$(uname)" = "Darwin"
-then
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-then
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-fi
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
 
 fi
 
@@ -20,7 +20,7 @@ echo "Updating package lists..."
 brew update
 
 # zsh install
-if test $(which zsh)
+if test $(which zsh) 
 then
 echo ''
 echo "zsh already installed..."
@@ -42,7 +42,7 @@ URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-complet
 echo ''
 echo "Downloading git-completion for git version: $GIT_VERSION..."
 if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
-echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
+	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
 fi
 
 # oh-my-zsh install
@@ -51,15 +51,15 @@ echo ''
 echo "oh-my-zsh is already installed..."
 read -p "Would you like to update oh-my-zsh now?" -n 1 -r
 echo ''
-if [[ $REPLY =~ ^[Yy]$ ]] ; then
-cd ~/.oh-my-zsh && git pull
-if [[ $? -eq 0 ]]
-then
-echo "Update complete..." && cd
-else
-echo "Update not complete..." >&2 cd
-fi
-fi
+    if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    cd ~/.oh-my-zsh && git pull
+        if [[ $? -eq 0 ]]
+        then
+            echo "Update complete..." && cd
+        else
+            echo "Update not complete..." >&2 cd
+        fi
+    fi
 else
 echo "oh-my-zsh not found, now installing oh-my-zsh..."
 echo ''
@@ -70,8 +70,8 @@ fi
 echo ''
 echo "Now installing oh-my-zsh plugins..."
 echo ''
-git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 # powerlevel9k install
@@ -91,7 +91,7 @@ echo ''
 echo "Now installing Pathogen..."
 echo ''
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Nerdtree for vim install
 echo ''
@@ -103,7 +103,7 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 echo ''
 echo "Now installing vim wombat color scheme..."
 echo ''
-git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat
+git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
 mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
 
 # Midnight commander install
@@ -112,39 +112,47 @@ echo "Now installing Midnight commander..."
 echo ''
 brew install mc
 
+# Fubectl install
+echo ''
+echo "Now installing Fubectl - fancy Kubectl..."
+curl -LO https://rawgit.com/kubermatic/fubectl/master/fubectl.source
+mv fubectl.source ~/.kube
+
 # Pull down personal dotfiles
 echo ''
-read -p "Do you want to use KDTheory's dotfiles? y/n" -n 1 -r
+read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-echo ''
-echo "Now pulling down KDTheory dotfiles..."
-git clone -b mac https://github.com/KDTheory/dotfiles.git ~/.dotfiles
-echo ''
-cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-echo ''
-echo "Checking out macOS branch..." && git checkout mac
-echo ''
-echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
-echo ''
+    echo ''
+	echo "Now pulling down jldeen dotfiles..."
+	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
+	echo ''
+	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+	echo ''
+	echo "Checking out macOS dev branch..." && git checkout mac-dev
+	echo ''
+	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+    echo ''
 
-if [[ $? -eq 0 ]]
-then
-echo "Successfully configured your environment with KDTheory's macOS dotfiles..."
-else
-echo "jldeen's macOS dotfiles were not applied successfully..." >&2
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully configured your environment with jldeen's macOS dotfiles..."
+    else
+        echo "jldeen's macOS dotfiles were not applied successfully..." >&2
 fi
-else
-echo ''
-echo "You chose not to apply KDTheory's macOS dotfiles. You will need to configure your environment manually..."
-echo ''
-echo "Setting defaults for .zshrc and .bashrc..."
-echo ''
-echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
-echo ''
-echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
-echo ''
-echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
-
+else 
+	echo ''
+    echo "You chose not to apply jldeen's macOS dotfiles. You will need to configure your environment manually..."
+	echo ''
+	echo "Setting defaults for .zshrc and .bashrc..."
+	echo ''
+	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+	echo ''
+	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
+	echo ''
+	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
+    echo ''
+    echo "[ -f ~/.kube/fubectl.source ] && source ~/.kube/fubectl.source" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added fubectl to .zshrc..."
+	
 fi
