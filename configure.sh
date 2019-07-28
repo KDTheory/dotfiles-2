e94972c7936632dc45170c73ce0bdb5049aaca70
KDTheory <kdarmondev@gmail.com>
2019-07-28 12:25:12 +0200

    Update wsl files
    Update from changes made on mac branches

--- configure.sh
+++ configure.sh
@@ -15,60 +15,138 @@ echo "Now installing git and bash-completion..."
 sudo apt-get install git bash-completion -y
 
 echo ''
-echo "Now configuring git-completion..."
-GIT_VERSION=`git --version | awk '{print $3}'`
-URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
-echo ''
-echo "Downloading git-completion for git version: $GIT_VERSION..."
-if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
-	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
-fi
+configureGitCompletion () {
+    GIT_VERSION=`git --version | awk '{print $3}'`
+    URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
+    success "git-completion for $GIT_VERSION downloaded"
+    if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
+        echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
+        fail 'git completion download failed'
+    fi
+}
 
-# oh-my-zsh install
-echo ''
-echo "Now installing oh-my-zsh..."
-echo ''
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ohmyzshInstall () {
+    # oh-my-zsh install
+    if [ -d ~/.oh-my-zsh/ ] ; then
+    info 'oh-my-zsh is already installed...'
+    read -p "Would you like to update oh-my-zsh now? y/n " -n 1 -r
+    echo ''
+        if [[ $REPLY =~ ^[Yy]$ ]] ; then
+        cd ~/.oh-my-zsh && git pull
+            if [[ $? -eq 0 ]]
+            then
+                success "Update complete..." && cd
+            else
+                fail "Update not complete..." >&2 cd
+            fi
+        fi
+    else
+    echo "oh-my-zsh not found, now installing oh-my-zsh..."
+    echo ''
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    success 'oh-my-zsh installed'
+    fi
+}
 
-# oh-my-zsh plugin install
-echo ''
-echo "Now installing oh-my-zsh plugins..."
-echo ''
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+ohmyzshPluginInstall () {
+    # oh-my-zsh plugin install
+    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]; then
+        info 'zsh-completions already installed'
+    else
+        git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions && success 'zsh-completions installed'
+    fi
+    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+        info 'zsh-autosuggestions already installed'
+    else
+        git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && success 'zsh-autosuggestions installed'
+    fi
+    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+        info 'zsh-syntax-highlighting already installed'
+    else
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && 'zsh-syntax-highlighting installed'
+    fi
+}
 
-# powerlevel9k install
-echo ''
-echo "Now installing powerlevel9k..."
-echo ''
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+pl9kInstall () {
+    # powerlevel9k install
+    if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel9k" ]; then
+        info 'powerlevel9k already installed'
+    else
+        echo "Now installing powerlevel9k..."
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k && success 'powerlevel9k installed'
+    fi
+}
 
-# vimrc vundle install
-echo ''
-echo "Now installing vundle..."
-echo ''
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+pl10kInstall () {
+    # powerlevel10k install
+    if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+        info 'powerlevel10k already installed'
+    else
+        echo "Now installing powerlevel10k..."
+        git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k && success 'powerlevel10k installed'
+    fi
+}
 
-# Pathogen install
-echo ''
-echo "Now installing Pathogen..."
-echo ''
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+tmuxTpmInstall () {
+    # tmux tpm install
+    if [ -d "$HOME/.tmux/plugins/tpm" ]; then
+        info 'tmux tpm already installed'
+    else
+        echo "Now installing Tmux TPM manager..."
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && success 'tmux tpm manager installed'
+    fi
+}
 
-# Nerdtree for vim install
-echo ''
-echo "Now installing Nerdtree for Vim..."
-echo ''
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+fubectlInstall () {
+    # fubectl install
+    # todo - move to after ~/bin check on bootstrap
+    if [ -f "$HOME/bin/fubectl.source" ]; then
+        info 'fubectl.source already exists'
+    else
+        echo "Now installing fubectl..."
+        curl -o "$HOME/bin/fubectl.source" -LO https://rawgit.com/kubermatic/fubectl/master/fubectl.source && success "fubectl placed in $HOME/bin"
+    fi
+}
 
-# Vim color scheme install
-echo ''
-echo "Now installing vim wombat color scheme..."
-echo ''
-git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
-mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
+vundleInstall () {
+    if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+        info 'vundle already exists'
+    else
+        # vimrc vundle install
+        echo ''
+        echo "Now installing vundle..."
+        echo ''
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && success 'vundle installed'
+    fi
+}
+
+pathogenInstall () {
+    if [ -f "$HOME/.vim/autoload/pathogen.vim" ]; then
+        info 'pathogen already installed'
+    else
+        mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+            curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && success 'pathogen installed'
+    fi
+}
+
+nerdtreeInstall () {
+    if [ -d "$HOME/.vim/bundle/nerdtree" ]; then
+        info 'vim nerdtree already installed'
+    else
+        git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree && success 'vim nerdtree installed'
+    fi
+}
+
+wombatColorSchemeInstall () {
+    if [ -f "$HOME/.vim/colors/wombat.vim" ]; then
+        info "wombat color scheme already installed"
+    else
+        # Vim color scheme install
+        git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
+        mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
+        success 'vim wombat color scheme installed'
+    fi
+}
 
 # Midnight commander install
 echo ''
@@ -93,13 +171,13 @@ mv dircolors.256dark .dircolors
 
 # Pull down personal dotfiles
 echo ''
-read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
+read -p "Do you want to use KDTheory's dotfiles? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo ''
-	echo "Now pulling down jldeen dotfiles..."
-	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
+	echo "Now pulling down KDTheory dotfiles..."
+	git clone https://github.com/KDTheory/dotfiles.git ~/.dotfiles
 	echo ''
 	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
 	echo ''
@@ -108,48 +186,23 @@ then
 	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
     if [[ $? -eq 0 ]]
     then
-        echo "Successfully configured your environment with jldeen's dotfiles..."
+        echo "Successfully configured your environment with KDTheory's dotfiles..."
     else
-        echo "jldeen's dotfiles were not applied successfully..." >&2
+        echo "KDTheory's dotfiles were not applied successfully..." >&2
 fi
 else 
 	echo ''
-    echo "You chose not to apply jldeen's dotfiles. You will need to configure your environment manually..."
+    echo "You chose not to apply KDTheory WSL dotfiles. You will need to configure your environment manually..."
 	echo ''
 	echo "Setting defaults for .zshrc and .bashrc..."
 	echo ''
-	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+	echo "source $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
 	echo ''
-	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
+	echo "source $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
 	echo ''
-	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
+    echo "[ -f ~/bin/fubectl.source ] && source ~/bin/fubectl.source" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added fubectl to .zshrc..."
 	
 fi
-
-# Setup and configure az cli
-echo ''
-read -p "Do you want to install Azure CLI? y/n (This will take some time...)" -n 1 -r
-echo ''
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Now installing az cli..."
-    AZ_REPO=$(lsb_release -cs)
-    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
-
-    sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-    sudo apt-get install apt-transport-https
-    sudo apt-get update && sudo apt-get install azure-cli
-	
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully installed Azure CLI 2.0."
-    else
-        echo "Azure CLI not installed successfully." >&2
-    fi
-    else 
-    echo "You chose not to install Azure CLI. Exiting now."
-    fi
 
 # Set default shell to zsh
 echo ''
